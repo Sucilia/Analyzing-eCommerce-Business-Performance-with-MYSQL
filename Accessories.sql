dc00138d0972771ac8dfa192dbf6c039e6109dc1
Sucilia <93129907+Sucilia@users.noreply.github.com>
2023-06-20 14:11:25 +0700

    Add files via upload

---
+++ Accessories.sql
@@ -0,0 +1,57 @@
+/*
+-- Query: SELECT * FROM edu_ecommerce_kel1.product where produk_id ='2'
+-- SELECT * FROM edu_ecommerce_kel1.product where produk_id ='3'
+-- SELECT * FROM edu_ecommerce_kel1.product where produk_id ='4'
+-- SELECT * FROM edu_ecommerce_kel1.product where produk_id ='5'
+-- SELECT * FROM edu_ecommerce_kel1.product where produk_id ='6'
+-- SELECT * FROM edu_ecommerce_kel1.product where produk_id ='7'
+-- SELECT * FROM edu_ecommerce_kel1.product where produk_id ='8'
+-- SELECT * FROM edu_ecommerce_kel1.product where produk_id ='9'
+-- SELECT * FROM edu_ecommerce_kel1.product where produk_id ='10'
+
+
+-- select count(*) from edu_ecommerce_kel1.product where produk_id <> 1
+-- select count(*) from edu_ecommerce_kel1.product where produk_id <> 3
+-- select count(*) from edu_ecommerce_kel1.product where produk_id <> 4
+-- select count(*) from edu_ecommerce_kel1.product where produk_id <> 5
+-- select count(*) from edu_ecommerce_kel1.product where produk_id <> 6
+-- select count(*) from edu_ecommerce_kel1.product where produk_id <> 7
+-- select count(*) from edu_ecommerce_kel1.product where produk_id <> 8
+-- select count(*) from edu_ecommerce_kel1.product where produk_id <> 9
+-- select count(*) from edu_ecommerce_kel1.product where produk_id <> 10
+LIMIT 0, 1000
+
+-- Date: 2023-06-20 13:26
+*/
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,'tas',' Accessories',75000,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,'sepatu',' Accessories',250000,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,'ikat Pinggang',' Accessories',20000,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,'kacamata',' Accessories',300000,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Hat L',' Accessories',362375,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Sunglasses N',' Accessories',1259218,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Wallet P',' Accessories',5361705,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Bracelet U',' Accessories',2613059,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Umbrella Y',' Accessories',535426,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Backpack F1',' Accessories',1343650,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Necklace I1',' Accessories',1571995,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Handbag M1',' Accessories',3704512,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Scarf O1',' Accessories',6562389,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Backpack R1',' Accessories',1935711,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Wallet T1',' Accessories',1165639,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Earrings U1',' Accessories',5966252,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Sunglasses Y1',' Accessories',4768836,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Wallet C2',' Accessories',6043635,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Backpack E2',' Accessories',4738867,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Watch G2',' Accessories',1364593,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Bracelet J2',' Accessories',2146178,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Hat P2',' Accessories',3765790,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Backpack R2',' Accessories',6394052,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Sunglasses V2',' Accessories',1625845,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Necklace Z2',' Accessories',2294662,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Umbrella A3',' Accessories',1678905,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Wallet C3',' Accessories',6705540,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Backpack E3',' Accessories',6524865,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Watch G3',' Accessories',499041,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Bracelet J3',' Accessories',3405483,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Hat P3',' Accessories',823473,500);
+INSERT INTO `` (`produk_id`,`product_name`,`category`,`price`,`inventory`) VALUES (2,' Backpack R3',' Accessories',1176704,500);
